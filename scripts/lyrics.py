d40585230803b45b7706a58c1aced06b822b036f
Tito-Osemobor <titoosemobor@gmail.com>
2023-01-22 01:30:26 -0500

    Updates

--- scripts/lyrics.py
+++ scripts/lyrics.py
@@ -1,15 +1,33 @@
-from flask import Flask, redirect, url_for, render_template
+import json
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
+from lyricsgenius import Genius
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+client_token = os.getenv("CLIENT_TOKEN")
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-def home ():
-  return render_template("index.html")
 
-@app.route("/practice", methods=["POST"])
-def lyrics ():
-  return render_template("practice.html")
+@app.route("/lyrics", methods=['POST'])
+def lyrics():
+  genius = Genius(client_token)
+  title = request.form['song']
+  singer = request.form['artist']
+  song = genius.search_song(title, singer)
+  if song.lyrics:
+    data = {"lyrics": song.lyrics}
+    json_data = json.dumps(data)
+    with open("song_lyrics.json", "w") as outfile:
+      outfile.write(json_data)
+      return "Lyrics successfully retrieved and stored in song_lyrics.json"
+  else:
+    print("This song doesn't have lyrics.")
 
-if __name__ == "__main__":
-  app.run(debug=True)
+
+if __name__ == '__main__':
+  app.run()
