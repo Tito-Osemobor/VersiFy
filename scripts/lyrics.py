10e5d5bcb533078d8b0d7772f44b520c65d41db0
Tito-Osemobor <titoosemobor@gmail.com>
2023-01-21 23:34:15 -0500

    Updates

--- scripts/lyrics.py
+++ scripts/lyrics.py
@@ -1,33 +1,15 @@
-import json
-from flask import Flask, request, render_template
-from dotenv import load_dotenv
-import os
-from lyricsgenius import Genius
-
-load_dotenv()
-
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-client_token = os.getenv("CLIENT_TOKEN")
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/lyrics', methods=['POST'])
-def lyrics():
-  genius = Genius(client_token)
-  title = request.form['song']
-  singer = request.form['artist']
-  song = genius.search_song(title, singer)
-  # if song.lyrics:
-  data = {"lyrics": song.lyrics}
-  json_data = json.dumps(data)
-  with open("song_lyrics.json", "w") as outfile:
-    outfile.write(json_data)
+@app.route("/")
+@app.route("/home")
+def home ():
+  return render_template("index.html")
+
+@app.route("/practice", methods=["POST"])
+def lyrics ():
   return render_template("practice.html")
-  # else:
-  #   return render_template("practice2.html")
 
-
-if __name__ == '__main__':
-  app.run()
+if __name__ == "__main__":
+  app.run(debug=True)
