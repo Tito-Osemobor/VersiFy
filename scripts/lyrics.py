eefa7bd16f31537d86ded2f69249efe6e9f5238d
Tito-Osemobor <titoosemobor@gmail.com>
2023-01-21 22:56:08 -0500

    Updates

--- scripts/lyrics.py
+++ scripts/lyrics.py
@@ -13,7 +13,7 @@ client_token = os.getenv("CLIENT_TOKEN")
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST'])
+@app.route("/lyrics", methods=['POST'])
 def lyrics():
   genius = Genius(client_token)
   title = request.form['song']
