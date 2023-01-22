0ec2c2c651cab6bf9104a2a050e827a646c6856f
maryam-fahmi <122686414+maryam-fahmi@users.noreply.github.com>
2023-01-21 21:52:05 -0400

    Installed Flask

--- scripts/lyrics.py
+++ scripts/lyrics.py
@@ -1,4 +1,5 @@
 import json
+from flask import Flask
 from dotenv import load_dotenv
 import os
 from lyricsgenius import Genius
@@ -9,15 +10,20 @@ client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 client_token = os.getenv("CLIENT_TOKEN")
 
-genius = Genius(client_token)
-singer = "The Weeknd"
-title = "In the night"
-# artist = genius.search_artist(singer, max_songs=3)
-song = genius.search_song(title, singer)
-if song.lyrics:
-    data = {"lyrics":song.lyrics}
+app = Flask(__name__)
+
+
+@app.route("/lyrics")
+def lyrics():
+  genius = Genius(client_token)
+  singer = "The Weeknd"
+  title = "In the night"
+  song = genius.search_song(title, singer)
+  if song.lyrics:
+    data = {"lyrics": song.lyrics}
     json_data = json.dumps(data)
     with open("song_lyrics.json", "w") as outfile:
-        outfile.write(json_data)
-else:
+      outfile.write(json_data)
+      print("This works")
+  else:
     print("This song doesn't have lyrics.")
